9e266e3d6607852064d3090d3b4ddd915b41c490
Michael Scholz <michael.scholz@draexlmaier.com>
2025-02-17 15:16:10 +0100

    Fixed contribution year in header

--- ichub-backend/models/example_drx.py
+++ ichub-backend/models/example_drx.py
@@ -1,7 +1,7 @@
 #################################################################################
 # Eclipse Tractus-X - Industry Core Hub Backend
 #
-# Copyright (c) DRÄXLMAIER Group
+# Copyright (c) 2025 DRÄXLMAIER Group
 # (represented by Lisa Dräxlmaier GmbH)
 # Copyright (c) 2025 Contributors to the Eclipse Foundation
 #
